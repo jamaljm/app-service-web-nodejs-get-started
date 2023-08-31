ba72ef167051b2639ce1455a305a531ad4a0831b
jamal <jamalvga2002@gmail.com>
2023-08-31 23:55:43 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const { createClient } = require('@supabase/supabase-js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/',async function(req, res, next) {
+  const options = {
+  db: {
+    schema: 'public',
+  },
+  auth: {
+    autoRefreshToken: true,
+    persistSession: false,
+    detectSessionInUrl: true,
+  },
+  global: {
+    headers: { 'x-my-custom-header': 'my-app-name' },
+  },
+};
+
+// Create a Supabase client
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY, options);
+const { data, error } = await supabase
+        .from('users')
+        .select('id');
+var server = http.createServer(app);
+console.log(data);
+
 });
 
 module.exports = router;
